50fa298b84aa5bdbf7072832d1ea1c37af42a50a
FrederikkeEbbe <febbe8@gmail.com>
2023-02-21 10:16:30 +0100

    burge menu der ikke virker endnu

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //Burger menu - Ebbe
+window.onload = function () {
 
+  const menu_knp = document.querySelector('.hbmenu');
+
+  menu_knp.addEventListener('klik', function () {
+    menu_knp.classList.toggle('aktivnu');
+  });
+}
 
 //Menukort
 
